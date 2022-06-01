f82b7cd71a470fbc0fcd94eb3b244bebe8cd4afe
Leticiavvs13 <leticiavsvieira@gmail.com>
2022-06-01 20:22:17 -0300

    Configuring Adapter

---
+++ app/src/main/java/br/com/etecia/mypets/ListaCachorroActivity.java
@@ -0,0 +1,54 @@
+package br.com.etecia.mypets;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+
+public class ListaCachorroActivity extends AppCompatActivity {
+
+    ListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.lista_cachorro_layout);
+
+        listView = findViewById(R.id.idListaCachorro);
+        MyAdapter adapter = new MyAdapter();
+        listView.setAdapter(adapter);
+    }
+
+    public class MyAdapter extends BaseAdapter {
+
+        @Override
+        public int getCount() {
+            return 0;
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            //TODO
+            String nome;
+            int image;
+
+
+
+            return null;
+        }
+    }
+
+}
